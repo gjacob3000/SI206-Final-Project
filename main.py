37ef534175ea7a0a1162375f12d346932efa0a16
gjac <gjac@umich.edu>
2020-11-30 02:16:25 -0500

    created a web pull from the air visual website as a roundabout, stores in database in batches of 25s

--- main.py
+++ main.py
@@ -22,15 +22,51 @@ def createTables(curr,conn):
     #curr.execute("DROP TABLE IF EXISTS CountryAQIs")
     curr.execute("CREATE TABLE IF NOT EXISTS CountryAQIs (name TEXT PRIMARY KEY, city TEXT, aqi INTEGER)")
 
-def getPollutionData():
+    curr.execute("CREATE TABLE IF NOT EXISTS AirWebAQIs (country TEXT PRIMARY KEY, aqi2019 NUMBER, aqi2018 NUMBER)")
+
+def getPollutionData(curr,conn):
     try:
+        curr.execute("SELECT country,aqi2019,aqi2018 FROM AirWebAQIs")
+        country_tuple = curr.fetchall()
+        countries = []
+        for country in country_tuple:
+            countries.append(country[0])
+        print(countries)
         url = "https://www.iqair.com/us/world-most-polluted-countries"
         soup = BeautifulSoup(requests.get(url).text, 'html.parser')
-        val = soup.find_all('td')
+        val = soup.find_all('tr')
+        count = 0 
+        for values in val:
+            if count < 25:
+                names = values.find_all('div', class_ = "country-name")
+                averages = values.find_all('span')
+                if len(names) == 0:
+                    continue
+                country = ""
+                for name in names:
+                    country =  name.getText()
+                    print(country)
+                aqi2019 = 0
+                aqi2018 = 0
+
+                if(averages[0].text != " - "):
+                    aqi2019 = float(averages[0].text)
+
+                if(averages[1].text != " - "):
+                    aqi2018 = float(averages[1].text)
+
+                print(aqi2019)
+                print(aqi2018)
+
+                if country not in countries:
+                    curr.execute("INSERT OR IGNORE INTO AirWebAQIs (country, aqi2019, aqi2018) VALUES (?,?,?)", (country, aqi2019, aqi2018))
+                    count+=1
         #print(val)
+
     except:
-        print("error when readong from url")
+        print("error when reading from url")
         dict_list = []
+    conn.commit()
     return
 
 def getPollutionApiData(curr,conn):
@@ -44,18 +80,21 @@ def getPollutionApiData(curr,conn):
                 url = "http://api.airvisual.com/v2/nearest_city?lat="+str(coord[1])+"&lon="+str(coord[2])+"&key="+AIRVISUALKEY
                 r = requests.get(url)
                 dict_list = json.loads(r.text)
-                
+                if dict_list["data"]["status"] == "fail":
+                    print("fail")
+                    conn.commit()
+                    return
                 covid_lat = coord[1]
                 covid_lon = coord[2]
                 aqi_lat = dict_list["data"]["location"]["coordinates"][1]
                 aqi_lon = dict_list["data"]["location"]["coordinates"][0]
                 aqi_close = isAQIClose(covid_lat, covid_lon, aqi_lat, aqi_lon)
                 
-                if aqi_close is False:
+                if aqi_close is False or (dict_list["data"] == "no_nearest_station"):
                     print(coord[0])
                     print(dict_list["data"]["country"])
                     print("wrong country!")
-                    removeFromData(curr, conn, coord[0])
+                    #removeFromData(curr, conn, coord[0])
                 else:
                     name = dict_list["data"]["country"]
                     city = dict_list["data"]["city"]
@@ -70,7 +109,7 @@ def getPollutionApiData(curr,conn):
                 
             run += 1
             #cant pull more than 10 per minute so we need to pause the system for a few seconds before collecting more
-            if run % 10 == 0 and run != 0: 
+            if run % 9 == 0 and run != 0: 
                 print("Pausing for a bit...")
                 time.sleep(60) # this pauses the program to give api a break
         conn.commit()    
@@ -78,6 +117,8 @@ def getPollutionApiData(curr,conn):
     except:
         print("error when reading from url")
         dict_list = []
+        conn.commit()  
+    conn.commit()  
     return
 
 def getCovidApiData(curr, conn):
@@ -122,7 +163,7 @@ def getCovidApiData(curr, conn):
         conn.commit()
 
     except:
-        print("error when accessing API")
+        print("error when accessing Covid API")
         dict_list = []
 
 #if any air quality points are too inaccurate we will remove the country from our analysis
@@ -151,8 +192,8 @@ def main():
     createTables(curr,conn)
     #loading API
     getCovidApiData(curr,conn)
-    getPollutionApiData(curr,conn)
-
+    #getPollutionApiData(curr,conn)
+    getPollutionData(curr,conn)
     print("hello world!")
 
 if __name__ == "__main__":
